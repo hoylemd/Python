2616bee8044801120977344966384a0e787703c9
Michael D. Hoyle <hoylemd@gmail.com>
2011-06-15 16:25:38 -0400

    started on arguments for random numbers, but its not working

--- Random Ints/RandomInts.py
+++ Random Ints/RandomInts.py
@@ -3,6 +3,39 @@
 
 import random
 import math
+import sys
+
+# load default settings
+list_length = 25
+lower_bound = -100
+upper_bound = 100
+
+# parse arguments
+argc = len(sys.argv)
+if argc > 1:
+	filename = sys.argv[1]
+else:
+	print "No output file provided. List will not be saved."
+	filename = ""
+	
+if argc > 2:
+	if sys.argv[0].isdigit():
+		list_length = int(sys.argv[2])
+	else:
+		print "List length invalid, defaulting to 25"
+		
+if argc > 3:
+	if sys.argv[0].isdigit():
+		lower_bound = int(sys.argv[3])
+	else:
+		print "Lower bound invalid, defaulting to -100"
+		
+if argc > 4:
+	if sys.argv[0].isdigit():
+		upper_bound = int(sys.argv[3])
+	else:
+		print "Upper bound invalid, defaulting to 100"
+
 
 list_length = 25
 lower_bound = -100
@@ -12,8 +45,22 @@ list_range = upper_bound - lower_bound
 # seed the random number generator
 random.seed()
 
-range(10)
+#initialize the list
+random_list = list()
 
 # generate the random ints
 for i in range(25):
-	print math.floor((random.random() * list_range) - lower_bound)
+	newNumber = int(math.floor((random.random() * list_range) + lower_bound))
+	random_list.append(newNumber)
+	
+# write the list to the file
+if filename != "":
+	outfile = open(filename, "w");
+	for x in random_list:
+		outfile.write(str(x) + "\n")
+	outfile.close()
+
+# print out the list
+print "List Generated:"
+for x in random_list:
+	print x
