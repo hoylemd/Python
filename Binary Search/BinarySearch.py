a2751f825c6cd6a6066a3547e2a73b2b1aeb4e9f
Michael D. Hoyle <hoylemd@gmail.com>
2011-06-21 15:49:23 -0400

    finished command-line argument parsing on Binary Search, and updated documentation accordingly

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -2,9 +2,18 @@
 
 import sys
 
+def isNumeric(number):
+	try:
+		float(number)
+		return True
+	except ValueError:
+		return False
+
+
+
 # initalize variables
-filename = ""
-target = 0
+filename = None
+target = None
 
 # parse arguments
 arg_count = len(sys.argv)
@@ -19,12 +28,26 @@ while current_arg < arg_count:
 		arg_value = sys.argv[current_arg+1]
 		
 		# parse input file
-		if arg_flag == "-i":
+		if arg_flag == "-f":
 			filename = arg_value
+			
+		# parse target
+		elif arg_flag == "-t":
+			if (isNumeric(arg_value)):
+				target = int(arg_value)
+			else:
+				print "Non-numeric target specified."
+				exit(3)
+		
+		# handle invalid flag
+		else:
+			print "Invalid argument flag provided."
+			exit(4)
 	else:
 		print "Mismatched arguments. Use -<flag> <argument>"
 		exit(2)
 
+	# increment argument pointer
 	current_arg += 2
 	
 # handle no arguments
@@ -32,4 +55,10 @@ if(arg_count == 1):
 	print "No arguments specifed."
 	exit(1)
 	
-print "filename: " + filename + " target: " + str(target)
+# handle unspecified arguments
+if filename == None:
+	print "No file specified to search."
+	exit(5)
+if target == None:
+	print "No target specified to search for."
+	exit(6)
--- Binary Search/readme.txt
+++ Binary Search/readme.txt
@@ -1,6 +1,16 @@
 Bianry Search Readme
 
+Calling syntax:
+BinarySearch.py <-f> <filename> <-t> <target value>
+
+The filename must be a valid path to a file containing a sorted list of integers.
+The target value must be an integer, but does not neccesarily have to exist in the given list.
+
 Error codes:
 0 - No error. Success
 1 - No arguments specifed
 2 - Flag not matched to argument or argument not matched to flag
+3 - Non-numeric target specified
+4 - Invalid argument flag
+5 - No file provided
+6 - No target provided
