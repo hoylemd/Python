72cbaef80165ae494ac47a53f061b62ae07935af
Michael D. Hoyle <hoylemd@gmail.com>
2011-07-09 17:21:06 -0400

    changelog

--- pygame/base_template.py
+++ pygame/base_template.py
@@ -1,14 +1,10 @@
-# Sample Python/Pygame Programs
-# Simpson College Computer Science
-# http://cs.simpson.edu 
+# Project name
+# module name
+# short description
+# by Michael D. Hoyle
+# Last modified: 7/9/2011
 
 import pygame 
-
-# Define some colors
-black    = (   0,   0,   0)
-white    = ( 255, 255, 255)
-green    = (   0, 255,   0)
-red      = ( 255,   0,   0) 
 
 pygame.init() 
 # Set the height and width of the screen
@@ -18,25 +14,24 @@ screen=pygame.display.set_mode(size)
 pygame.display.set_caption("My Game") 
 
 #Loop until the user clicks the close button.
-done=False 
+quit=False 
 
 # Used to manage how fast the screen updates
-clock=pygame.time.Clock() 
+clock=pygame.time.Clock()
 
-# -------- Main Program Loop -----------
-while done==False:
-    for event in pygame.event.get(): # User did something
-		if event.type == pygame.QUIT: # If user clicked close
-			done=True # Flag that we are done so we exit this loop
+# Main Program Loop
+while !quit==False:
 
-	# Set the screen background
-    screen.fill(black)
+	# Handle events
+    for event in pygame.event.get():
+		if event.type == pygame.QUIT: # exit
+			quit=True
 
 	# Limit to 20 frames per second
-    clock.tick(20)     
+    clock.tick(30)     
 	
-	# Go ahead and update the screen with what we've drawn.    pygame.display.flip()
+	# update the screen.
+	pygame.display.flip()
 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
+# Be IDLE friendly.
 pygame.quit ()
