a6c6860dcf2046f9a36aba055b681e8facbb4e9a
Michael D. Hoyle <hoylemd@gmail.com>
2011-07-09 19:58:53 -0400

    pygame/Mousepractice:  - added the box class  - added drawing a box where the user clicks  - added collision detection for boxes  - added clicking to remove a box  - made boxes pop up centered on the clicking spot

--- pygame/MousePractice/MousePractice.py
+++ pygame/MousePractice/MousePractice.py
@@ -8,6 +8,40 @@
 import pygame 
 from hutils import colours
 
+# box sprite
+class Box(pygame.sprite.Sprite):
+	def __init__(self, colour, initial_position):
+		# initialize base class
+		pygame.sprite.Sprite.__init__(self)
+		
+		# make the surface
+		self.image = pygame.Surface([15,15])
+		self.image.fill(colour)
+		
+		# position it
+		self.rect = self.image.get_rect()
+		self.rect.topleft = initial_position
+
+# wrapper function to blit a sprite
+def drawSprite(s):
+	screen.blit(s.image, s.rect)
+	
+# check if a box exists at the given position
+def boxHere(position):
+	for b in boxes:
+		if b.rect.collidepoint(position):
+			return True
+	return False
+		
+# get a box at the current position
+def getBoxAt(position):
+	for b in boxes:
+		if b.rect.collidepoint(position):
+			return b
+	
+	return None
+
+# initialize pygame
 pygame.init() 
 
 # Set the height and width of the screen
@@ -23,7 +57,7 @@ clock=pygame.time.Clock()
 
 # game variables
 clickPosition = (0,0)
-
+boxes = []
 
 #Loop until the user clicks the close button.
 quit=False 
@@ -39,7 +73,14 @@ while quit == False:
 		if event.type == pygame.QUIT: # exit
 			quit=True
 		if event.type == pygame.MOUSEBUTTONDOWN: # click
-			clickPosition = pygame.mouse.get_pos()	
+			clickPosition = pygame.mouse.get_pos()
+			theBox = getBoxAt(clickPosition)
+			if theBox != None:
+				boxes.remove(theBox)
+			else:
+				boxPosition = (clickPosition[0] - 7, clickPosition[1] -7)
+				boxes.append(Box(colours.blue, boxPosition))
+			
 
 	# render text
 	clickPosImage = font.render(str(clickPosition), True, colours.black)
@@ -51,6 +92,10 @@ while quit == False:
 	# draw text
 	screen.blit(clickPosImage, [0,0])
 	
+	# draw boxes
+	for b in boxes:
+		drawSprite(b)
+		
 	# update the screen.
 	pygame.display.flip()
 
